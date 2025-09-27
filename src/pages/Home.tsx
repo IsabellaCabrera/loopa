100063768891dde70889178f35cd59f19c4c8415
ItsSilva <juanse909@hotmail.com>
2025-09-27 18:18:45 -0500

    Update: InformativeCard add to home pag

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import { NavBar } from "../components/Header/NavBar";
-import { SearchBar } from "../components/SearchBar";
 import { PromosCard } from "../components/Cards/Promos";
 import { RestaurantCard } from "../components/Cards/Restaurant";
+import { InformativeCard } from "../components/Cards/Informative";
+import { Input } from "../components/Input";
 
 const promoInfo = [
   {
@@ -48,33 +49,71 @@ const restaurant = [
   },
 ];
 
+const loopaInfo = [
+  {
+    id: crypto.randomUUID(),
+    logo: "/loopalogo.svg",
+    text: "50% in most restaurants",
+    background: "/Informativebgramen.webp",
+  },
+  {
+    id: crypto.randomUUID(),
+    logo: "/loopalogo.svg",
+    text: "50% in most restaurants",
+    background: "/Informativebgtaco.webp",
+  },
+];
+
 export const Home = () => {
   return (
     <>
       <NavBar />
-      <SearchBar />
-            <section className="py-9 px-12">
-              <h2 className="font-bold text-xl">The best promos!</h2>
-              <section className="mb-8 grid grid-cols-1 sm:grid-cols-2  gap-3 mt-3">
-                {promoInfo.map(({ id, img, restaurant, avatar, promo }) => (
-                  <PromosCard
-                    key={id}
-                    img={img}
-                    restaurantAvatar={avatar}
-                    restuarant={restaurant}
-                    promo={promo}
-                  />
-                ))}
-              </section>
-            </section>
-                  <section className="py-9 px-12">
-                    <h2 className="font-bold text-xl">Top 10 Picks!</h2>
-                    <section className="grid grid-flow-col justify-between gap-4 mt-3 overflow-x-auto scrollbar-hide">
-                      {restaurant.map(({ id, img, restaurant }) => (
-                        <RestaurantCard key={id} img={img} restaurant={restaurant} />
-                      ))}
-                    </section>
-                  </section>
+      <section className="h-[440px] bg-[url(/herohomeimg.webp)] bg-center bg-cover bg-no-repeat" />
+      <section className="my-9 mx-12">
+        <Input
+          type="search"
+          placeholder="Search for restaurants, dishes..."
+          name="search"
+        />
+      </section>
+      {/* PromosCard */}
+      <section className="py-9 px-12">
+        <h2 className="font-bold text-xl">The best promos!</h2>
+        <section className="mb-8 grid grid-cols-1 sm:grid-cols-2  gap-3 mt-3">
+          {promoInfo.map(({ id, img, restaurant, avatar, promo }) => (
+            <PromosCard
+              key={id}
+              img={img}
+              restaurantAvatar={avatar}
+              restuarant={restaurant}
+              promo={promo}
+            />
+          ))}
+        </section>
+      </section>
+      {/* RestaurantCard */}
+      <section className="py-9 px-12">
+        <h2 className="font-bold text-xl">Top 10 Picks!</h2>
+        <section className="grid grid-flow-col justify-between gap-4 mt-3 overflow-x-auto scrollbar-hide">
+          {restaurant.map(({ id, img, restaurant }) => (
+            <RestaurantCard key={id} img={img} restaurant={restaurant} />
+          ))}
+        </section>
+      </section>
+      {/* InformativeCard */}
+      <section className="py-9 px-12">
+        <h2 className="font-bold text-xl">Did you know?</h2>
+        <section className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-3">
+          {loopaInfo.map(({ id, logo, text, background }) => (
+            <InformativeCard
+              key={id}
+              logo={logo}
+              text={text}
+              background={background}
+            />
+          ))}
+        </section>
+      </section>
     </>
   );
 };
