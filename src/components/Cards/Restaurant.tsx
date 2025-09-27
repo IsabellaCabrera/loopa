dcd8a316c99d3b5b4c89d8efa3d33ecb0df93e73
ItsSilva <juanse909@hotmail.com>
2025-09-27 16:58:56 -0500

    Fix: Use a normal HTML image to load the image

--- src/components/Cards/Restaurant.tsx
+++ src/components/Cards/Restaurant.tsx
@@ -4,9 +4,13 @@ export const RestaurantCard = ({ img, restaurant }: RestaurantCardProps) => {
   return (
     <article>
       <figure className="flex flex-col gap-2 items-center justify-center">
-        <div
-          className={`rounded-full w-32 h-32 bg-[url(/${img})] bg-cover bg-center bg-no-repeat`}
-        />
+        <div className="rounded-full w-32 h-32 overflow-hidden">
+          <img 
+            src={img} 
+            alt={restaurant}
+            className="w-full h-full object-cover"
+          />
+        </div>
         <p className="text-primary font-medium">{restaurant}</p>
       </figure>
     </article>
