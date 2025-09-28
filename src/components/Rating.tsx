6ff8f27d599b3cb6ec47d9837a1c129c4f8f97c0
ItsSilva <juanse909@hotmail.com>
2025-09-28 01:47:38 -0500

    Add: Rating component

---
+++ src/components/Rating.tsx
@@ -0,0 +1,11 @@
+import type { RatingProps } from "../types/rating";
+import { FaStar } from "react-icons/fa";
+
+export const Rating = ({ value }: RatingProps) => {
+  return (
+    <div className="flex items-center gap-1 bg-[#EABF754D] px-2 py-1 rounded-full">
+      <FaStar className="text-[#F9CE69]" size={16} />
+      <span className="text-gray-800 text-sm font-medium">{value}</span>
+    </div>
+  );
+};
