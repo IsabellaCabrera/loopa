75199ffb2f6a2be6fc39f1d374722774a35ef8d7
María José Rodríguez Martínez <mariajorm2004@gmail.com>
2025-09-28 18:17:26 -0500

    Update: Navbar and input componets

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,14 +5,14 @@ export const Input = ({ label, type, name, placeholder }: InputProps) => {
   return (
     <div className="flex flex-col gap-2 overflow-clip w-full">
       {label && <label className="font-medium" htmlFor={name}>{label}</label>}
-      <div className="p-2 border border-morado/20 rounded-md flex items-center gap-2">
+      <div className="p-2 border border-morado/50 rounded-md flex items-center gap-2">
         {type === "search" && (
           <i>
             <SearchIcon />
           </i>
         )}
         <input
-          className=" focus:outline-none flex-1 placeholder:text-morado/20"
+          className=" focus:outline-none flex-1 placeholder:text-black/45"
           type={type === "search" ? "text" : type}
           name={name}
           placeholder={placeholder}
