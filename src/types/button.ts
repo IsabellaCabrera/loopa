179293646d9be780bc14560003c13e82411d2202
ItsSilva <juanse909@hotmail.com>
2025-09-28 00:25:51 -0500

    Add: CheckboxProps for Checkbox component

--- src/types/button.ts
+++ src/types/button.ts
@@ -4,3 +4,11 @@ export type ButtonProps = {
   secondary?: boolean;
   children: React.ReactNode;
 }
+
+export type CheckboxProps = {
+  id: string;
+  label: string;
+  checked: boolean;
+  img?: string;
+  onChange: (checked: boolean) => void;
+};
