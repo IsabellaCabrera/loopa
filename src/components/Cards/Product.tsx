0ac46ca7570f3c487ffd3806ca09f283c67fcc26
Isa-Cabrera-S <isabella.cabrera@u.icesi.edu.co>
2025-09-29 14:04:08 -0500

    update: Product card add navagate to button

--- src/components/Cards/Product.tsx
+++ src/components/Cards/Product.tsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router"
 import { Button } from "../Button"
 
 export const ProductCard = () => {
+    const navigate = useNavigate()
+
+    
     return (
         <article className="flex flex-col gap-6 p-4 bg-white rounded-2xl">
             <img className="h-[140px] w-auto object-cover rounded-lg " src="/mclogo.webp" alt="" />
@@ -8,7 +12,7 @@ export const ProductCard = () => {
                 <h3 className="font-bold">Product Name</h3>
                 <p className="text-morado font-bold text-2xl flex items-center gap-1">$6.99<span className="text-black/30 font-normal text-xl">(Save $6)</span></p>
             </div>
-            <Button>Add</Button>
+            <Button onClick={()=> navigate("/customer/checkout")}>Add</Button>
         </article>
     )
 }
