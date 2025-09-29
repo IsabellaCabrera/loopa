9b481ae96eb4e5ed15c67a282926cd1614e6f666
Majo Rodríguez <mariajorm2004@gmail.com>
2025-09-29 12:11:10 -0500

    Update: Restaurant card new prop new variant

--- src/components/Cards/Restaurant.tsx
+++ src/components/Cards/Restaurant.tsx
@@ -1,14 +1,14 @@
 import type { RestaurantCardProps } from "../../types/cards";
 
-export const RestaurantCard = ({ img, restaurant }: RestaurantCardProps) => {
+export const RestaurantCard = ({ img, restaurant, whiteVariant }: RestaurantCardProps) => {
   return (
     <article>
       <figure className="flex flex-col gap-2 items-center justify-center">
         <div
         style={{backgroundImage : `url(${img})`}}
-          className={`rounded-full w-32 h-32 bg-cover bg-center bg-no-repeat`}
+          className={`rounded-full w-32 h-32 bg-cover bg-center bg-no-repeat ring-5 ring-white/20`}
         />
-        <p className="text-primary font-medium">{restaurant}</p>
+        <p className={`font-medium ${whiteVariant ? "text-mainWhite" :"text-morado"}`}>{restaurant}</p>
       </figure>
     </article>
   );
